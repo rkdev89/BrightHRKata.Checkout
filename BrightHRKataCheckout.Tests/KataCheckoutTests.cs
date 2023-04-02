bb3f2f38a7e872c61f3fda77d15961ad15f6ec5b
rkdev89 <richardkemp35@gmail.com>
2023-04-02 20:12:25 +0100

    Red - Checkout receives a single item but does not give correct price

--- BrightHRKataCheckout.Tests/KataCheckoutTests.cs
+++ BrightHRKataCheckout.Tests/KataCheckoutTests.cs
@@ -20,5 +20,22 @@ namespace BrightHRKataCheckout.Tests
             //Assert
             Assert.That(price, Is.EqualTo(0));
         }
+
+        [Test]
+        [TestCase("A", 50)]
+        [TestCase("B", 30)]
+        [TestCase("C", 20)]
+        [TestCase("D", 15)]
+        public void CheckoutReceivesSingleItemAndReturnsPrice(string item, int price)
+        {
+            //Arrange
+            var checkout = new Checkout();
+
+            //Act
+           var result = checkout.Scan(item);
+
+            //Assert
+            Assert.That(price, Is.EqualTo(result));
+        }
     }
 }
