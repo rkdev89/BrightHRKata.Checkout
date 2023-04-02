8c62b56bc30276356f83898387f6edf4d87bee04
rkdev89 <richardkemp35@gmail.com>
2023-04-02 20:13:56 +0100

    Green - make Scan method pass for a single string item

--- BrightHRKata.Checkout/Checkout.cs
+++ BrightHRKata.Checkout/Checkout.cs
@@ -8,7 +8,18 @@
             {
                 return 0;
             }
-            return 1;
+            switch (item)
+            {
+                case "A":
+                    return 50;
+                case "B":
+                    return 30;
+                case "C":
+                    return 20;
+                case "D":
+                    return 15;
+            }
+            return -1;
         }
     }
 }
