f4308bdb50d2f59781c106fd7b4b8836a0c14b01
rkdev89 <richardkemp35@gmail.com>
2023-04-02 21:48:20 +0100

    Refactor - Create discounts for both A and B skus at the same time

--- BrightHRKataCheckout.Tests/KataCheckoutTests.cs
+++ BrightHRKataCheckout.Tests/KataCheckoutTests.cs
@@ -75,5 +75,43 @@ namespace BrightHRKataCheckout.Tests
             //Assert
             Assert.That(result, Is.EqualTo(total));
         }
+
+        [Test]
+        public void CheckoutScansTwoBsAndAppliesADiscount()
+        {
+            //Arrange
+            var checkout = new Checkout();
+            var result = 45;
+
+            //Act
+            checkout.AddDiscount(new Discount { SkuName = "B", Threshold = 2, Value = 15 });
+            checkout.Scan(new Sku { Name = "B", Price = 30 });
+            checkout.Scan(new Sku { Name = "B", Price = 30 });
+            var total = checkout.GetTotal();
+
+            //Assert
+            Assert.That(result, Is.EqualTo(total));
+        }
+
+        [Test]
+        public void CheckoutScansThreesAsAndTwoBsAndAppliesDiscountToBoth()
+        {
+            //Arrange
+            var checkout = new Checkout();
+            var result = 175;
+
+            //Act
+            checkout.AddDiscount(new Discount { SkuName = "A", Threshold = 3, Value = 20 });
+            checkout.AddDiscount(new Discount { SkuName = "B", Threshold = 2, Value = 15 });
+            checkout.Scan(new Sku { Name = "A", Price = 50 });
+            checkout.Scan(new Sku { Name = "A", Price = 50 });
+            checkout.Scan(new Sku { Name = "A", Price = 50 });
+            checkout.Scan(new Sku { Name = "B", Price = 30 });
+            checkout.Scan(new Sku { Name = "B", Price = 30 });
+            var total = checkout.GetTotal();
+
+            //Assert
+            Assert.That(result, Is.EqualTo(total));
+        }
     }
 }
